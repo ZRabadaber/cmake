5716c14ccbfd01079759e77ec50001a5ef5fb70c
Андрей <zr@paradise.zrabadaber.gmail.com.beta.tailscale.net>
2022-10-13 00:18:04 +0300

    disable build shared libs

--- arm-none-eabi.cmake
+++ arm-none-eabi.cmake
@@ -4,7 +4,8 @@ list(APPEND CMAKE_MODULE_PATH ${CMAKE_CURRENT_LIST_DIR})
 # Perform compiler test with static library
 set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
 
-add_definitions(-DBUILD_SHARED_LIBS=OFF)
+set(BUILD_SHARED_LIBS FALSE CACHE "disable build shared libs")
+
 set(CMAKE_C_FLAGS   "${MCPU_FLAGS} ${VFP_FLAGS} ${SPEC_FLAGS} -fdata-sections -ffunction-sections -mlong-calls" CACHE INTERNAL "c compiler flags")
 set(CMAKE_CXX_FLAGS "${MCPU_FLAGS} ${VFP_FLAGS} ${SPEC_FLAGS} -fdata-sections -ffunction-sections -fno-rtti -fno-exceptions -mlong-calls" CACHE INTERNAL "cxx compiler flags")
 set(CMAKE_ASM_FLAGS "${MCPU_FLAGS} ${VFP_FLAGS} ${SPEC_FLAGS} -x assembler-with-cpp" CACHE INTERNAL "asm compiler flags")
