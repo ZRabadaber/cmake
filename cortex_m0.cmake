f0bbe3cfe94283effe9d1758f88c7a6b70e74d8d
Андрей Тараскин <a.taraskin@rlt.ru>
2022-12-19 15:08:22 +0300

    add -mthumb-interwork del --specs=nano.specs

--- cortex_m0.cmake
+++ cortex_m0.cmake
@@ -2,9 +2,9 @@
 set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_SYSTEM_PROCESSOR ARM)
 
-set(MCPU_FLAGS "-mthumb -mcpu=cortex-m0")
+set(MCPU_FLAGS "-mthumb -mcpu=cortex-m0 -mthumb-interwork")
 set(VFP_FLAGS "-mfloat-abi=hard -mfpu=fpv4-sp-d16")
-set(SPEC_FLAGS "--specs=nano.specs")
+set(SPEC_FLAGS "")
 set(LD_FLAGS "--specs=nosys.specs")
 
 include(${CMAKE_CURRENT_LIST_DIR}/arm-none-eabi.cmake)
--- cortex_m4.cmake
+++ cortex_m4.cmake
@@ -2,9 +2,9 @@
 set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_SYSTEM_PROCESSOR ARM)
 
-set(MCPU_FLAGS "-mthumb -mcpu=cortex-m4")
+set(MCPU_FLAGS "-mthumb -mcpu=cortex-m4 -mthumb-interwork")
 set(VFP_FLAGS "-mfloat-abi=hard -mfpu=fpv4-sp-d16")
-set(SPEC_FLAGS "--specs=nano.specs")
+set(SPEC_FLAGS "")
 set(LD_FLAGS "--specs=nosys.specs")
 
 include(${CMAKE_CURRENT_LIST_DIR}/arm-none-eabi.cmake)
